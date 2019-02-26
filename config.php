038624aceb6dc4d935f8aa1c2de9faa7f5739b1a
Eric Rosário <ericsantos.r@hotmail.com>
2019-02-26 14:33:55 -0300

    Merge Completed

--- config.php
+++ config.php
@@ -5,29 +5,6 @@ define("BASE_URL", "http://localhost/gestao_php/");
 
 global $config;
 $config = array();
-<<<<<<< HEAD
-if (ENVIRONMENT == 'development') {
-    $config['base_url'] = 'http://localhost/gestao_php/';
-    $config['dbname'] = '';
-    $config['host'] = 'localhost';
-    $config['dbuser'] = 'root';
-    $config['dbpass'] = '';
-} else {
-    $config['base_url'] = 'http://meusite.com.br/';
-    $config['dbname'] = '';
-    $config['host'] = 'localhost';
-    $config['dbuser'] = 'root';
-    $config['dbpass'] = '';
-}
-global $db;
-try {
-    $db = new PDO("mysql:dbname=" . $config['dbname'] . "host=" . $config['host'], $config['dbuser'], $config['dbpass'], $config['base__url']);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "ERRO: " . $e->getMessage();
-    exit;
-}
-=======
 if(ENVIRONMENT == 'development') {
 	$config['dbname'] = 'erp';
 	$config['host'] = 'localhost';
@@ -40,4 +17,3 @@ if(ENVIRONMENT == 'development') {
 	$config['dbpass'] = '';
 }
 ?>
->>>>>>> 8fdbedb6744e074dbb6e57209d6fb327482c36d2
--- environment.php
+++ environment.php
@@ -1,8 +1,5 @@
 <?php
 
-<<<<<<< HEAD:enviroment.php
-$config("ENVIRONMENT","deselopment");
-$config("ENVIRONMENT", "production");
-=======
 define("ENVIRONMENT","deselopment");
->>>>>>> 8fdbedb6744e074dbb6e57209d6fb327482c36d2:environment.php
+
+?>
