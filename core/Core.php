ad8ad511f6df988e9e147d415dbf59160de75861
Eric Rosário <ericsantos.r@hotmail.com>
2019-02-26 14:30:24 -0300

    Resolvendo conflitos da branch

--- core/Core.php
+++ core/Core.php
@@ -1,39 +1,4 @@
 <?php
-<<<<<<< HEAD
-class Core
-{
-    public function run()
-    {
-        $url = '/';
-        if (isset($_GET['url'])) {
-            $url .= $_GET['url'];
-        }
-
-        if (!empty($url) && $url != '/') {
-            $url = explode('/', $url);
-            array_shift($url);
-
-            $currentController = $url[0] . 'Controller';
-            array_shift($url);
-          
-            if (isset($url[0]) && !empty($url[0])) {
-                $currentAction = $url[0];
-            } else {
-                $currentAction = 'index';
-            }
-            if (count($url) > 0) {
-                $params = $url;
-            }
-        } else {
-            $currentController = 'homeController';
-            $currentAction = 'index';
-        }
-      
-        $c = new $currentController();
-        call_user_func_array(array($c, $currentAction), $params);
-    } 
-  
-=======
 class Core {
 
 	public function run() {
@@ -69,5 +34,4 @@ class Core {
 
 	}
 
->>>>>>> 8fdbedb6744e074dbb6e57209d6fb327482c36d2
 }
--- core/controller.php
+++ core/controller.php
@@ -1,30 +1,4 @@
 <?php
-<<<<<<< HEAD
-// Houve remoção do public pois ela só deve ser utilizado caso tivesse criado alguma classe aqui
-// Coloquei dentro da função os require junto com as variavéis pois estavam nos parametros 
-//mas não estavam sendo utilizadas e que indicam as rotas analisando a sua lógica
-
-function loadView($viewName, $viewData) 
-{
-    require 'views/' . $viewName . 'php';
-    require 'views/' . $viewData . 'php';
-
-}
-function index()
-{
-    require 'views/' . $viewName . 'php';
-}
-
-function loadTamplate($viewName, $viewData = array())
-{
-    require 'views/template.php';
-}
-
-function loadViewInTemplate($viewName, $viewData = array())
-{
-    extract($viewData);
-    require 'view/' . $viewName . 'php';
-=======
 class controller {
 
 	protected $db;
@@ -48,5 +22,4 @@ class controller {
 		include 'views/'.$viewName.'.php';
 	}
 
->>>>>>> 8fdbedb6744e074dbb6e57209d6fb327482c36d2
 }
